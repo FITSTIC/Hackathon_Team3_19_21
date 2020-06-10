c84b2b87469b6bd7670e2aae053efb43ee5fcaf3
johnvillalb <63226318+johnvillalb@users.noreply.github.com>
2020-06-10 10:16:32 +0200

    Aggior. Class.cs Modulo e Esame

--- HackatonOnline/HackatonOnline/Class.cs
+++ HackatonOnline/HackatonOnline/Class.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -50,5 +51,74 @@ namespace HackatonOnline
             this.Città = Città;
         }
     }
-    
-}
+    public class Esame
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Punteggio { get; set; }
+        [Required]
+        public string Modulo { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public int StudenteId { get; set; }
+        [Required]
+
+
+
+
+        public Esame (int Id, int Punteggio, string Modulo, DateTime Data, int StudenteId)
+        {
+            this.Id = Id;
+            this.Punteggio = Punteggio;
+            this.Modulo = Modulo;
+            this.Data = Data;
+            this.StudenteId = StudenteId;
+
+        }
+    }
+    public class Modulo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime DataInizio { get; set; }
+        public DateTime DataFine {get; set;}
+        public string Materia { get; set; }
+        public string Nome { get; set; }
+        [Required]
+        public int CorsoId { get; set; }
+        [Required]
+        public int DocenteId { get; set; }
+        [Required]
+        public int TutorId { get; set; }
+        [Required]
+
+
+        public Modulo(int Id, DateTime DataInizio, DateTime DataFine, string Materia, string Nome, int CorsoId, int DocenteId, int TuttorId)
+        {
+            this.Id = Id;
+            this.DataInizio = DataInizio;
+            this.DataFine = DataFine;
+            this.Materia = Materia;
+            this.Nome = Nome;
+            this.CorsoId = CorsoId;
+            this.DocenteId = DocenteId;
+            this.TutorId = TutorId;
+
+        }
+
+    }
+
+
+
+
+
+
+}   
+
+
+ 
+
+
