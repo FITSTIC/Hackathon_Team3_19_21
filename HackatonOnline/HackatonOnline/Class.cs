e600f54d2cb264305bea2d76c4bb286541590dd2
Andrea Latino <36225035+AndreL34@users.noreply.github.com>
2020-06-10 10:07:04 +0200

    Creazione Classi pt.2

--- HackatonOnline/HackatonOnline/Class.cs
+++ HackatonOnline/HackatonOnline/Class.cs
@@ -50,5 +50,172 @@ namespace HackatonOnline
             this.Città = Città;
         }
     }
-    
+    public interface Personale
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cognome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public int Telefono { get; set; }
+        public DateTime DataNascita { get; set; }
+        [Required]
+        public DateTime DataAssunzione { get; set; }
+    }
+    public class Organizzatore : Personale
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cognome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public int Telefono { get; set; }
+        public DateTime DataNascita { get; set; }
+        [Required]
+        public DateTime DataAssunzione { get; set; }
+        public Organizzatore(int Id, string Nome, string Cognome, string Email, int Telefono, DateTime DataNascita, DateTime DataAssunzione)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Email = Email;
+            this.Telefono = Telefono;
+            this.DataNascita = DataNascita;
+            this.DataAssunzione = DataAssunzione;
+        }
+        public Organizzatore(int Id, string Nome, string Cognome, string Email, DateTime DataAssunzione)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Email = Email;
+            this.DataAssunzione = DataAssunzione;
+        }
+    }
+    public class Docente : Personale
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cognome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public int Telefono { get; set; }
+        public DateTime DataNascita { get; set; }
+        [Required]
+        public DateTime DataAssunzione { get; set; }
+        public Docente(int Id, string Nome, string Cognome, string Email, int Telefono, DateTime DataNascita, DateTime DataAssunzione)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Email = Email;
+            this.Telefono = Telefono;
+            this.DataNascita = DataNascita;
+            this.DataAssunzione = DataAssunzione;
+        }
+        public Docente(int Id, string Nome, string Cognome, string Email, DateTime DataAssunzione)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Email = Email;
+            this.DataAssunzione = DataAssunzione;
+        }
+    }
+    public class Tutor : Personale
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cognome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public int Telefono { get; set; }
+        public DateTime DataNascita { get; set; }
+        [Required]
+        public DateTime DataAssunzione { get; set; }
+        public Tutor(int Id, string Nome, string Cognome, string Email, int Telefono, DateTime DataNascita, DateTime DataAssunzione)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Email = Email;
+            this.Telefono = Telefono;
+            this.DataNascita = DataNascita;
+            this.DataAssunzione = DataAssunzione;
+        }
+        public Tutor(int Id, string Nome, string Cognome, string Email, DateTime DataAssunzione)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Email = Email;
+            this.DataAssunzione = DataAssunzione;
+        }
+    }
+    public class Corso
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public DateTime DataInizio { get; set; }
+        public DateTime DataFine { get; set; }
+        [Required]
+        public string Sede { get; set; }
+        [Required]
+        public string AnnoAccademico { get; set; }
+        public int OrganizzatoreId { get; set; }
+
+        public Corso(int Id, string Nome, DateTime DataInizio, DateTime DataFine, string Sede, string AnnoAccademico, int OrganizzatoreId)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.DataInizio = DataInizio;
+            this.DataFine = DataFine;
+            this.Sede = Sede;
+            this.AnnoAccademico = AnnoAccademico;
+            this.OrganizzatoreId = OrganizzatoreId;
+        }
+        public Corso(int Id, string Nome, DateTime DataInizio, string Sede, string AnnoAccademico, int OrganizzatoreId)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.DataInizio = DataInizio;
+            this.Sede = Sede;
+            this.AnnoAccademico = AnnoAccademico;
+            this.OrganizzatoreId = OrganizzatoreId;
+        }
+
+    }
+    public class Iscrizione
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int CorsoId { get; set; }
+        [Required]
+        public int StudenteId { get; set; }
+        [Required]
+        public DateTime DataIscrizione { get; set; }
+        public Iscrizione(int Id, int Corso, int Studente, DateTime DataIscrizione)
+        {
+            this.Id = Id;
+            this.CorsoId = Corso;
+            this.StudenteId = Studente;
+            this.DataIscrizione = DataIscrizione;
+        }
+    }
 }
